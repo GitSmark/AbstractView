84f4c2364626a25b1be5f6d12c7198a31c1b9a5d
huangxy <630686026@qq.com>
2016-08-03 11:07:14 +0800

    Update AbstractFragmentActivity.java

--- AbstractFragmentActivity.java
+++ AbstractFragmentActivity.java
@@ -1,64 +1,98 @@
-package com.ttt.abstracts;
+package com.administrator.abstractviewsample.AbstractView;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 /**
- * Created by huangxy on 2016/3/4.
+ * Created by huangxy on 2016/3/3.
+ * https://github.com/GitSmark/AbstractView
  */
-public abstract class AbstractFragmentActivity extends FragmentActivity implements OnClickListener{
+public abstract class AbstractFragmentActivity extends FragmentActivity implements OnClickListener {
 
-	public abstract void initView();
+	protected abstract int  getLayoutResId();
+	protected abstract void initView();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		BeforeCreate();
 		super.onCreate(savedInstanceState);
-		AfterCreate();
+		setContentView(getLayoutResId());
+		beforeInitView();
 		initView();
 		getData();
 		Other();
 	}
-	
-	public View setClick(int id){
-		View view = findViewById(id);
+
+	@SuppressWarnings("uncheckedz")
+	 protected <T extends View> T $find(int Resid) {
+		return (T) findViewById(Resid);
+	}
+
+	@SuppressWarnings("uncheckedz")
+	protected <T extends View> T $find(View v, int Resid) {
+		return (T) v.findViewById(Resid);
+	}
+
+	@SuppressWarnings("uncheckedz")
+	protected <T extends View> T $click(int Resid) {
+		T t = (T) findViewById(Resid);
+		try {t.setOnClickListener(this);
+		}catch (Exception e){}
+		return t;
+	}
+
+	@SuppressWarnings("uncheckedz")
+	protected <T extends View> T $click(View v, int Resid) {
+		T t = (T) v.findViewById(Resid);
+		try {t.setOnClickListener(this);
+		}catch (Exception e){}
+		return t;
+	}
+
+	@SuppressWarnings("uncheckedz")
+	protected <T extends View> T $onClick(int Resid, OnClickListener listener) {
+		T t = (T) findViewById(Resid);
+		try {t.setOnClickListener(listener);
+		}catch (Exception e){}
+		return t;
+	}
+
+	@SuppressWarnings("uncheckedz")
+	protected <T extends View> T $onClick(View v, int Resid, OnClickListener listener) {
+		T t = (T) v.findViewById(Resid);
+		try {t.setOnClickListener(listener);
+		}catch (Exception e){}
+		return t;
+	}
+
+	protected void $Title(int id, String text){
 		try {
-			view.setOnClickListener(this);
-		}catch (RuntimeException e){
+			TextView tv = (TextView) findViewById(id);
+			tv.setText(text);
+		}catch (Exception e){
 
 		}
-		return view;
 	}
-	
-//	public void setTitle(int id, String text){
-//		try {
-//			TextView tv = (TextView) findViewById(id);
-//			tv.setText(text);
-//		}catch (Exception e){
-//
-//		}
-//	}
-//
-//	public void setFinish(int id){
-//		try {
-//			findViewById(id).setOnClickListener(new OnClickListener() {
-//				@Override
-//				public void onClick(View v) {
-//					BeforeFinish();
-//					finish();
-//				}
-//			});
-//		}catch (Exception e){
-//
-//		}
-//	}
-//
-//	public void BeforeFinish() {}
-	public void BeforeCreate() {}
-	public void AfterCreate() {}
+
+	protected void $Finish(int id){
+		try {
+			findViewById(id).setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					beforeFinish();
+					finish();
+				}
+			});
+		}catch (Exception e){
+
+		}
+	}
+
+	public void beforeInitView() {}
+	public void beforeFinish() {}
 	public void getData() {}
 	public void Other() {}
 
